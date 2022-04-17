9372e4d38fd0664fb66a7a95e23f961e8445591d
MidNightRider666 <ledas850@gmail.com>
2022-04-17 12:31:54 +0300

    Add NotFound Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
+import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Add from './pages/Add/Add';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import NotFound from './pages/NotFound';
 import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Switch>
       <Route path={'/'} exact>
       <Register />
       </Route>
@@ -23,6 +26,10 @@ function App() {
       <Route path={'/Add'}>
         <Add />
       </Route>
+      <Route path={'*'}>
+        <NotFound />
+      </Route>
+      </Switch>
     </div>
   );
 }
